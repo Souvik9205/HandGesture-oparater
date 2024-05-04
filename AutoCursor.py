3ab367dd504b5f336c9eb180a1a4cc4b3e33712e
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-05-05 01:21:40 +0530

    mouse-move , clicks, drag-drop

---
+++ AutoCursor.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import HandtrackingModule as htm
+import pyautogui
+
+wCam, hCam = 640, 480
+frameR = 100
+smoothening = 5
+pinch_threshold = 20
+
+pLocX, pLocY = 0, 0
+cLocX, cLocY = 0, 0
+isHolding = False
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+detector = htm.handDetector(maxHands=1)
+wScr, hScr = pyautogui.size()
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img)
+
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        length12, img, lineInfo = detector.findDistance(8, 12, img)
+        length01, img, lineInfo1 = detector.findDistance(4, 8, img)
+        length02, img, lineInfo2 = detector.findDistance(4, 12, img)
+        length03, img, lineInfo3 = detector.findDistance(4, 16, img)
+        length04, img, lineInfo4 = detector.findDistance(4, 20, img)
+
+        print(length01, length02, length03, length04)
+
+        fingers = detector.fingersUp()
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (0, 0, 255), 2)
+
+    # GESTURES LISTS ARE HERE -------------------------------------
+
+        # [CURSOR MODE]
+        if fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0 and fingers[0] == 0:
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
+            cLocX = pLocX + (x3 - pLocX) / smoothening
+            cLocY = pLocY + (y3 - pLocY) / smoothening
+
+            pyautogui.moveTo(cLocX, cLocY)
+            cv2.circle(img, (x1, y1), 12, (255, 255, 51), 3, 8, 0)
+            pLocX, pLocY = cLocX, cLocY
+
+        # [CLICK MODES]
+        # LEFT CLICK MODE HERE ---------------
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0 and fingers[3] == 0 and fingers[4] == 0:
+            if length12 < 25:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 6, (0, 255, 0), cv2.FILLED)
+                pyautogui.click(button='left')
+
+        # RIGHT CLICK MODE HERE ---------------
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 1 and fingers[3] == 0 and fingers[4] == 0:
+            if length12 < 25:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 6, (0, 255, 0), cv2.FILLED)
+                pyautogui.click(button='right')
+
+        # [DRAG AND DROP MODE]
+        if (
+            length01 < pinch_threshold and
+            length02 < pinch_threshold and
+            length03 < pinch_threshold and
+            length04 < pinch_threshold
+        ):
+            if(isHolding == False):
+                pyautogui.mouseDown(button='left')
+                isHolding = True
+                pyautogui.moveTo(x3, y3)
+        else:
+            if(isHolding == True):
+                pyautogui.mouseUp(button='left')
+                isHolding = False
+
+
+    cv2.imshow("image", img)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- HandtrackingModule.py
+++ HandtrackingModule.py
@@ -1,14 +1,17 @@
 import cv2
 import mediapipe as mp
-import time
+import math
+import numpy as np
+
 class handDetector():
-    def __init__(self,mode=False, maxHands=2):
+    def __init__(self, mode=False, maxHands=2):
         self.mode = mode
         self.maxHands = maxHands
 
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands)
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
 
     def findHands(self, img, draw = True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -22,23 +25,57 @@ class handDetector():
         return img
 
     def findPosition(self,img, handNo=0, draw= True):
-        lmList = []
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
 
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append([id, cx, cy])
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (77, 77, 255), cv2.FILLED)
 
-        return lmList
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (65, 65, 65), 1)
+            # cv2.circle(img, (x1, y1), 2, (255, 0, 255), cv2.FILLED)
+            # cv2.circle(img, (x2, y2), 2, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 2, (75, 255, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
-    pTime = 0
     cap = cv2.VideoCapture(0)
-
     detector = handDetector()
 
     while True:
@@ -46,18 +83,19 @@ def main():
         img = cv2.flip(img, 1)
         img = cv2.resize(img, (640, 480))
         img = detector.findHands(img)
-        lmList = detector.findPosition(img)
+        lmList, bbox = detector.findPosition(img)
 
         if len(lmList) != 0:
-            print(lmList[4])
+          print(lmList[4])
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 87, 51), 2)
         cv2.imshow("image", img)
-        cv2.waitKey(1)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
